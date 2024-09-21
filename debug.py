74196deb2968e9aa91ccca64b27a595dc47e0795
Rene <racosta323@gmail.com>
2024-09-21 17:14:39 -0500

    creates Person class and creates attributes

--- debug.py
+++ debug.py
@@ -1,6 +1,9 @@
 from main import PdfDoc
+from main import Person
 import ipdb
 
-pdf = PdfDoc("https://www.sos.state.tx.us/elections/forms/vr-with-receipt.pdf")
+# pdf = PdfDoc("https://www.sos.state.tx.us/elections/forms/vr-with-receipt.pdf")
+
+me = Person()
 
 ipdb.set_trace()
--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from io import BytesIO
 from fillpdf import fillpdfs
 
 class PdfDoc():
-    def __init__(self,url,pdf=None, pdf_content=None, form_fields=[]):
+    def __init__(self,url,pdf=None, pdf_content=None, form_fields=[], dict_fields={}):
         self.url = url
         self._pdf = pdf
         self._pdf_content = pdf_content
-        self._form_fields = form_fields
+        self._form_fields = form_fields #dict
 
 
     def request(self):
-        
+
         try:
             request = requests.get(self.url)
             if request.status_code == 200:
@@ -25,6 +25,47 @@ class PdfDoc():
     def get_fields(self):
         
         self.request()
-        self.form_fields = list(fillpdfs.get_form_fields(self.pdf_content).keys())
+        self.form_fields = fillpdfs.get_form_fields(self.pdf_content)
+        # self.form_fields = list(fillpdfs.get_form_fields(self.pdf_content).keys())
 
-        
+
+
+class Person():
+    def __init__(self,**kwargs):
+        self._attributes = {
+            "first_name":None,
+            "last_name":None,
+            "middle_name":None,
+            "former_name":None,
+            "street_address":None,
+            "city":None,
+            "county":None,
+            "zip_code":None,
+            "street_address_2":None,
+            "city_2":None,
+            "state":None,
+            "zip_code_2":None,
+            "former_res":None,
+            "birth_month":None,
+            "birth_day":None,
+            "birth_year":None,
+            "phone":None,
+            "phone_2":None,
+            "phone_3":None,
+            "license":None,
+            "ssn":None,
+            "no_id":None,
+            "citizenship":None,
+            "voting_age":None,
+            "election_worker":None,
+            "gender":None,
+        }
+
+
+        for key, value in kwargs.items():
+            if key in self._attributes:
+                self._attributes[key] = value
+            else:
+                raise AttributeError(f'Invalid attribute: {key}')
+
+   
