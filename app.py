bed75a83fac1a5fc6d6e2fe79e12f254dadd7069
Rene <racosta323@gmail.com>
2024-09-24 12:35:04 -0500

    adds logic to menu to collect user input

--- app.py
+++ app.py
@@ -1,84 +1,78 @@
 import ipdb
+from prompts import prompts, mailing_address_prompts
+
+space = ' '
 
 def exit_program():
-    print("\nYour changes are not saved. Goodbye.\n")
+    print(f"\n{space*10}Your changes are not saved. Goodbye.\n")
     exit()
 
 
 def edit_entries(user_info):
     while True:
-        print("\nPlease review your entries:")
-        for key, value in user_info.items():
-            print(f"{key.capitalize()}: {value}")
+        # for prompt in prompts:
+        #     print(f"{prompts.index(prompt)+1}{space*10}{prompt}")
 
-        edit = input("\nWould you like to edit any of your entries? (Y/N): ").strip().upper()
+        print(f"\n{space*10}Please review your entries:\n")
+        ipdb.set_trace()
+        if user_info:
+            for key, value in user_info.items():    
+                print(f"{space*10}{key}. {prompts[key-1]}: {value}")
+        else:
+            for prompt in prompts:
+                print(f'{space*10}{prompts.index(prompt)+1}. {prompt}')
+
+
+        edit = input(f"\n{space*10}Would you like to edit any of your entries? (Y or N): ").strip().upper()
         if edit == 'Y':
-            key_to_edit = input("Which entry would you like to edit? (Enter the key): ").strip().lower()
+            key_to_edit = input(f"{space*10}\nWhich entry would you like to edit? (Enter the associated number): ")
             if key_to_edit in user_info:
-                new_value = input(f"Enter the new value for {key_to_edit}: ")
+                new_value = input(f"{space*10}Enter the new value for {key_to_edit}: ")
                 user_info[key_to_edit] = new_value
             else:
-                print("Invalid key. Please enter a valid key.")
+                print(f"{space*10}Invalid key. Please enter a valid key.")
         else:
             break
         
         print(user_info)
 
+
 def get_mailing_address():
-
-    mailing_address_prompts = [
-        "What is your mailing address (if different than your residence address)? ",
-        "Residence address city? ",
-        "Residence address state? ",
-        "Residence address zip code? ",
-    ]
-
     mailing_address_info = {}
-    
     
     for prompt in mailing_address_prompts:
         while True:
-            response = input(f'{prompt}')
+            response = input(f'{prompts.index(prompt)+1}{prompt}')
             if response == 'q':
                 exit_program()
                 
             if response.strip():
-                mailing_address_info[prompt.split()[0].lower()] = input(f"{prompt}")
+                mailing_address_info[prompts.index(prompt)+1] = input(f"{prompt}")
                 break
-            else:
-                print("This field cannot be left blank. Please enter a valid response.")
+            # else:
+            #     print("This field cannot be left blank. Please enter a valid response.")
 
     return mailing_address_info
 
 
 def menu():
-    print("\nHi -- Welcome.\n")
-
-    prompts = [
-        "What is your first name? ",
-        "What is your last name? ",
-        "What is your middle name, if any? ",
-        "Do you have a former name? (e.g. a maiden name) ",
-        "What is your street address IN TEXAS, including any apartment numbers? If none, describe where you live. ",
-        "What city (in TX)? ",
-        "What county (in TX)? ",
-        "Do you have a mailing address different than the one described (Y/N)? ",
-    ]
+    print(f"\n{space*10}Hi -- Welcome.\n")
 
     user_info = {}
 
     for prompt in prompts:
-        ipdb.set_trace()
         while True:
-            response = input(f'{prompt}')
+            response = input(f'{space*10}{prompts.index(prompt)+1}. {prompt}')
             if response == 'q':
                 exit_program()
             
             if response.strip():
-                user_info[prompt.split()[0].lower()] = response
+                user_info[prompts.index(prompt)+1] = response
                 break
-            else:
-                print("This field cannot be left blank. Please enter a valid response.")
+            if not response:
+                break
+            # else:
+            #     print("This field cannot be left blank. Please enter a valid response.")
 
             # secondary mailing address
             if (
@@ -92,9 +86,8 @@ def menu():
             ):
                 mailing_address_info = get_mailing_address()
                 user_info.update(mailing_address_info)
-                ipdb.set_trace()
                 
-            user_info = edit_entries(user_info)
+    user_info = edit_entries(user_info)
 
     return user_info
 
---
+++ prompts.py
@@ -0,0 +1,54 @@
+# key_mapping = {
+#         'first_name': "What is your first name? ",
+#         'last_name': "What is your last name? ",
+#         'middle_name': "What is your middle name, if any? ",
+#         'former_name': "Do you have a former name? (e.g. a maiden name) ",
+#         'street_address': "What is your street address IN TEXAS, including any apartment numbers? If none, describe where you live. ",
+#         'city': "What city (in TX)? ",
+#         'county': "What county (in TX)? ",
+#         'mailing_address': "Do you have a mailing address different than the one described (Y or N)? ",
+#         'mailing_address_city': 'What city? ',
+#         'mailing_address_state': 'What state? ',
+#         'mailing_address_zip': 'Zip code? ',
+#         'former_res': 'What is the city AND county of your former residence IN TEXAS? ',
+#         'birth_month': 'What month were you born? ',
+#         'birth_day': 'What day were you born? ',
+#         'birth_year': 'What year were you born? ',
+#         'phone': 'What is your phone number? ',
+#         'license': 'What is your TX driver\'s license number OR TX Personal ID number? ',
+#         'ssn': 'What are the LAST 4 digits of your social security number? ',
+#         'citizen': 'Are you a United State Citizen (Y or N)? ',
+#         'age': 'Will you be 18 years of age on or before election day? ',
+#         'election_worker': 'Are you interested in becoming an election worker (Y or N)? '
+# }
+
+prompts = [
+        "What is your first name? ",
+        "What is your last name? ",
+        # "What is your middle name, if any? ",
+        # "Do you have a former name? (e.g. a maiden name) ",
+        # "What is your street address IN TEXAS, including any apartment numbers? If none, describe where you live. ",
+        # "What city (in TX)? ",
+        # "What county (in TX)? ",
+        # "Do you have a mailing address different than the one described (Y or N)? ",
+        # 'What city? ',
+        # 'What state? ',
+        # 'Zip code? ',
+        # 'What is the city AND county of your former residence IN TEXAS? ',
+        # 'What month were you born? ',
+        # 'What day were you born? ',
+        # 'What year were you born? ',
+        # 'What is your phone number? ',
+        # 'What is your TX driver\'s license number OR TX Personal ID number? ',
+        # 'What are the LAST 4 digits of your social security number? ',
+        # 'Are you a United State Citizen (Y or N)? ',
+        # 'Will you be 18 years of age on or before election day? ',
+        # 'Are you interested in becoming an election worker (Y or N)? '
+]
+
+mailing_address_prompts = [
+        "What is your mailing address (if different than your residence address)? ",
+        "Residence address city? ",
+        "Residence address state? ",
+        "Residence address zip code? ",
+    ]
