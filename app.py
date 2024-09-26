d9f8bd7ab112e03e6b6a50eee6f7ffe8427e0d7a
Rene <racosta323@gmail.com>
2024-09-26 12:29:04 -0500

    updates edit entries method to account for prompts obj

--- app.py
+++ app.py
@@ -9,24 +9,18 @@ def exit_program():
     exit()
 
 def preview_changes(user_info):
-    for prompt in prompts:
-        prompt_keys = prompts.index(prompt)
-        user_info_keys = list(user_info.keys())
-        
-        print(f"{space*10}{prompt_keys+1}. {prompt}: {user_info[prompt_keys+1]}")
+    for i, (prompt, value) in enumerate(prompts.items(), start = 1):
+        print(f"{space*10}{i}. {prompt} {user_info[prompt]}")
         
 
 def edit_entries(user_info):
     while True:
-        # for prompt in prompts:
-        #     print(f"{prompts.index(prompt)+1}{space*10}{prompt}")
-        
         print(f"\n{space*10}{ast*50}\n")
         print(f"{space*10}PLEASE REVIEW YOUR ENTRIES:\n")
         
         preview_changes(user_info)
                 
-
+        # need to work on this:
         print(f"\n{space*10}{ast*50}\n")
         edit = input(f"\n{space*10}Would you like to edit any of your entries? (Y or N): ").strip().upper()
         if edit == 'Y':
@@ -59,37 +53,56 @@ def get_mailing_address():
     return mailing_address_info
 
 
+# def menu():
+#     print(f"\n{space*10}Hi -- Welcome.\n")
+
+#     user_info = {}
+
+#     for prompt in prompts:
+#         while True:
+#             response = input(f'{space*10}{prompts.index(prompt)+1}. {prompt}')
+#             if response == 'q':
+#                 exit_program()
+            
+#             if response.strip():
+#                 user_info[prompts.index(prompt)+1] = response
+#                 break
+#             if not response:
+#                 break
+#             # else:
+#             #     print("This field cannot be left blank. Please enter a valid response.")
+
+#             # secondary mailing address
+#             if (
+#                 prompt.startswith(
+#                     "Do you have a mailing address different than the one described (Y/N)? "
+#                 )
+#                 and user_info[
+#                     "do you have a mailing address different than the one described (y/n)? "
+#                 ]
+#                 == "Y"
+#             ):
+#                 mailing_address_info = get_mailing_address()
+#                 user_info.update(mailing_address_info)
+                
+#     user_info = edit_entries(user_info)
+
+#     return user_info
+
 def menu():
     print(f"\n{space*10}Hi -- Welcome.\n")
 
     user_info = {}
-
-    for prompt in prompts:
+    
+    for prompt, value in prompts.items():
         while True:
-            response = input(f'{space*10}{prompts.index(prompt)+1}. {prompt}')
-            if response == 'q':
-                exit_program()
+            response = input(f'{space*10}{prompt}')
             
-            if response.strip():
-                user_info[prompts.index(prompt)+1] = response
-                break
-            if not response:
-                break
-            # else:
-            #     print("This field cannot be left blank. Please enter a valid response.")
-
-            # secondary mailing address
-            if (
-                prompt.startswith(
-                    "Do you have a mailing address different than the one described (Y/N)? "
-                )
-                and user_info[
-                    "do you have a mailing address different than the one described (y/n)? "
-                ]
-                == "Y"
-            ):
-                mailing_address_info = get_mailing_address()
-                user_info.update(mailing_address_info)
+            if response.lower() == 'q':
+                exit_program()
+                
+            user_info[prompt] = response
+            break
                 
     user_info = edit_entries(user_info)
 
