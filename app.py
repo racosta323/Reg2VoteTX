7e8faa9413a997be12ca92f1c54fd9e5b580cf4d
Rene <racosta323@gmail.com>
2024-09-26 19:46:19 -0500

    updates menu logic to account for new user_info obj

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import ipdb
 from prompts import prompts, mailing_address_prompts
 from main import Person
+from main import PdfDoc
 
 space = ' '
 ast = '*'
@@ -11,9 +12,10 @@ def exit_program():
 
 def preview_changes(user_info):
     for i, (prompt, value) in enumerate(prompts.items(), start = 1):
-        print(f"{space*10}{i}. {prompt} {user_info[prompt]}")
+        key = prompts[prompt]
+        # ipdb.set_trace()
+        print(f"{space*10}{i}. {prompt} {user_info.get(key)}")
         
-
 def edit_entries(user_info):
     while True:
         print(f"\n{space*10}{ast*50}\n")
@@ -35,24 +37,27 @@ def edit_entries(user_info):
                 print("Invalid input. Please enter a valid number")
         else:
             #what happens if you don't want to edit
-            user_info_keys = user_info.keys()
-            person_info = {}
-            
-            for key, value in user_info.items():
-                if key in user_info_keys:
-                    person_info[prompts[key]] = user_info[key]
-                    
-            person = Person(**person_info)
-                    
             ipdb.set_trace()
-            
-            # for key in user_info_keys:
-            #     print(prompts(key))
-            
-            
+            person = create_person(**user_info)
+            url = "https://www.sos.state.tx.us/elections/forms/vr-with-receipt.pdf"
+            write_pdf(person, url)
             break
         
-
+def create_person(self, user_info):
+    user_info_keys = user_info.keys()
+    person_info = {}
+    
+    for key, value in user_info.items():
+        if key in user_info_keys:
+            person_info[prompts[key]] = user_info[key]
+            
+    person = Person(**person_info)
+    return person
+    
+def write_pdf(self, person, url):
+    pdf = PdfDoc(url)
+    pdf.write_pdf(person)
+        
 def get_mailing_address():
     mailing_address_info = {}
     
@@ -70,43 +75,6 @@ def get_mailing_address():
 
     return mailing_address_info
 
-
-# def menu():
-#     print(f"\n{space*10}Hi -- Welcome.\n")
-
-#     user_info = {}
-
-#     for prompt in prompts:
-#         while True:
-#             response = input(f'{space*10}{prompts.index(prompt)+1}. {prompt}')
-#             if response == 'q':
-#                 exit_program()
-            
-#             if response.strip():
-#                 user_info[prompts.index(prompt)+1] = response
-#                 break
-#             if not response:
-#                 break
-#             # else:
-#             #     print("This field cannot be left blank. Please enter a valid response.")
-
-#             # secondary mailing address
-#             if (
-#                 prompt.startswith(
-#                     "Do you have a mailing address different than the one described (Y/N)? "
-#                 )
-#                 and user_info[
-#                     "do you have a mailing address different than the one described (y/n)? "
-#                 ]
-#                 == "Y"
-#             ):
-#                 mailing_address_info = get_mailing_address()
-#                 user_info.update(mailing_address_info)
-                
-#     user_info = edit_entries(user_info)
-
-#     return user_info
-
 def menu():
     print(f"\n{space*10}Hi -- Welcome.\n")
 
@@ -118,8 +86,9 @@ def menu():
             
             if response.lower() == 'q':
                 exit_program()
-                
-            user_info[prompt] = response
+            
+            #need to update this so that keys look like py keys    
+            user_info[prompts[prompt]] = response
             break
                 
     user_info = edit_entries(user_info)
