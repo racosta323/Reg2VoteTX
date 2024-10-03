56cfa1e004338f14c9e54c65b25a3b942aee8536
Rene <racosta323@gmail.com>
2024-10-03 18:34:36 -0500

    lays out address section in form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,8 @@ function App() {
 
   return (
     <div class='flex justify-center items-center min-h-screen bg-gray-100'>
-      <div class="w-full max-w-4xl h-96 bg-white shadow-md overflow-hidden md:max-w-7xl md:flex rounded "> 
+      <div class="w-full max-w-4xl bg-white shadow-md md:max-w-7xl md:flex rounded "> 
         <Form />       
-        {/* <div class='bg-black text-white p-4 '>
-          hello
-        </div> */}
-        <div class='bg-white p-4'>
-          other
-        </div>
       </div>
     </div>
   )
